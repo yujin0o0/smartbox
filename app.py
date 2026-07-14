e7cb844634722fc506e57c3755270cb8d275fdbb
yujin0o0 <24-10127@gsuite.gen.go.kr>
2026-07-15 00:52:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -19,3 +19,43 @@ st.write("https://malleable-verdict-0bb.notion.site/a943a7db7654827b882a01be2d3c
 # 6. 푸터(Footer) 및 안내
 st.markdown("---")
 st.caption("⚙️ 제작: [yujin] (UI/UX 및 스트림릿 기반 노코드 웹앱 프로토타입)") 
+st.markdown("""
+<style>
+
+.boom{
+    position:fixed;
+    font-size:55px;
+    animation: explode 2.5s ease-out forwards;
+    z-index:9999;
+}
+
+.b1{left:10%; top:80%;}
+.b2{left:30%; top:75%;}
+.b3{left:50%; top:85%;}
+.b4{left:70%; top:70%;}
+.b5{left:90%; top:80%;}
+
+@keyframes explode{
+    0%{
+        transform:translateY(0) scale(0.2) rotate(0deg);
+        opacity:1;
+    }
+    40%{
+        transform:translateY(-250px) scale(1.8) rotate(180deg);
+        opacity:1;
+    }
+    100%{
+        transform:translateY(-450px) scale(3) rotate(360deg);
+        opacity:0;
+    }
+}
+
+</style>
+
+<div class="boom b1">🚨</div>
+<div class="boom b2">🚨</div>
+<div class="boom b3">🚨</div>
+<div class="boom b4">🚨</div>
+<div class="boom b5">🚨</div>
+
+""", unsafe_allow_html=True)
